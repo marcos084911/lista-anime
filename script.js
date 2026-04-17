b87f0951476317b450a2ba3cae7cf57f62b26c52
M4rcosAbraao <marc0s084911@gmail.com>
2026-04-16 22:39:29 -0300

    .

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function showTab(name, el) {
-    document.querySelectorAll('.chapter').forEach(c => c.classList.remove('active'));
-    document.querySelectorAll('.nav-tab').forEach(t => t.classList.remove('active'));
-    document.getElementById('tab-' + name).classList.add('active');
-    el.classList.add('active');
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
